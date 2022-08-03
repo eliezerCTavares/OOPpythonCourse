09728d8d3b526c1e1550ea23e273d7f2dc481d04
Eliezer Tavares <tavares.eliezer@yahoo.com>
2022-08-03 00:41:19 -0300

    Create OOP_01.py
    # First time creating a class header

---
+++ OOP_01.py
@@ -0,0 +1,34 @@
+# Create 3 classes that could be included in a system designed for a:
+# - Small Bank.
+# - Donut Shop.
+# - Videogame Store.
+
+# - Small Bank:
+class ClientAccount:
+    pass
+
+class Deposit:
+    pass
+
+class Loan:
+    pass
+
+# - Donut Shop:
+class Donut:
+    pass
+
+class Client:
+    pass
+
+class Employee:
+    pass
+
+# - Videogame Store:
+class Videogame:
+    pass
+
+class Game:
+    pass
+
+class Acessories:
+    pass
